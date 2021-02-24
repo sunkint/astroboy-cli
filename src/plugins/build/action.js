94d9fbbb1ed4ac72ccb78235194d4ac852229b1a
sunkint <sunkint@yeah.net>
2021-02-24 22:29:28 +0800

    fix: 去掉 build 文件拷贝步骤

--- src/plugins/build/action.js
+++ src/plugins/build/action.js
@@ -14,15 +14,4 @@ module.exports = function(command) {
     shell.exit(-1);
   }
   console.log("========== 结束 TS 编译 ==========");
-
-  console.log("========== 开始文件拷贝 ==========");
-  result = shell.cp("-R", "./app/views", "./dist/app/views");
-  if (result.code !== 0) {
-    shell.exit(-1);
-  }
-  result = shell.cp("./config/*.json", "./dist/config");
-  // if (result.code !== 0) {
-  //   shell.exit(-1);
-  // }
-  console.log("========== 结束文件拷贝 ==========");
 };
